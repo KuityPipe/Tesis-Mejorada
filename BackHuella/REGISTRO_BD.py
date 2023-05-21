76b348799abb1c3edc8f3c24cbb55d4ccf9b4eaf
Kuity <106512613+SrKuity@users.noreply.github.com>
2023-05-21 21:39:49 +0000

    21_05_2023 Felipe

--- BackHuella/REGISTRO_BD.py
+++ BackHuella/REGISTRO_BD.py
@@ -2,10 +2,14 @@ import CONEXION_BD
 from datetime import datetime
 import hashlib
 import sys
+from flask import Flask, request
+
+app = Flask(__name__)
 
 #LLAMAR A LA BASE DE DATOS
 cursora = CONEXION_BD.cur
 
+"""
 #CREAR NUEVO USUARIO Y INSERTAR DATOS
 Rut = int(input("Introduce rut: "))
 Nombre = str(input("Introuce el nombre: "))
@@ -20,29 +24,52 @@ Direccion = str(input("Introuce el direccion: "))
 Tipo_cuenta = int(input("Introduce el tipo de cuenta a adquirir: [1]gratis [2]individual [3]pyme_pequeña [4]pyme_mediana [5]empresarial "))
 Contraseña = str(input("Introduce contraseña: "))
 Contraseña2 = str(input("Vuelve a introducir la contraseña: "))
+"""
 
-if Contraseña == Contraseña2 :
+@app.route('Pag/paginicio', methods=['POST'])
+def registro():
+    #RECOGER DATOS DEL FORMULARIO
+    Rut = int(request.form.get('rut'))
+    Nombre = request.form.get('nombre')
+    Nombre2 = request.form.get('nombre2')
+    Apellido = request.form.get('apellido')
+    Apellido2 = request.form.get('apellido2')
+    Edad = int(request.form.get('edad'))
+    Telefono = int(request.form.get('telefono'))
+    Email = request.form.get('email')
+    Comuna = int(request.form.get('comuna'))
+    Direccion = request.form.get('direccion')
+    Tipo_cuenta = int(request.form.get('tipo_cuenta'))
+    Contraseña = request.form.get('password')
+    Contraseña2 = request.form.get('password_confirm')
+
+  #REVISAR QUE LAS CONTRASEÑAS COINCIDAN
+    if Contraseña == Contraseña2 :
         hash_object = hashlib.sha256(Contraseña.encode())
         Contraseña = hash_object.hexdigest()
-        print(Contraseña)
-else:
-        print("Contraseña no coincide adios")
-        sys.exit()
+    else:
+        return 'Contraseña no coincide adios', 400
+    
+  #CREAR TIMESTAMP
+    TimeStamp = datetime.now()
+    TimeStamp = TimeStamp.strftime("%y-%m-%d %H:%M:%S")
+    print("Usuario creado a las ", TimeStamp)
 
-TimeStamp = datetime.now()
-TimeStamp = TimeStamp.strftime("%y-%m-%d %H:%M:%S")
-print("Usuario creado a las ", TimeStamp)
+    #INSERTAR DATOS EN LA BASE DE DATOS
+    sql = "INSERT INTO TRANSACCION(TIEMPO_TRANSACCION, FK_VALOR_CUENTA) VALUES(%s,%s)"
+    val = (TimeStamp,Tipo_cuenta)
+    cursora.execute(sql,val)
+    cursora.connection.commit()
+    sql = "SELECT ID_TRANSACCION FROM TRANSACCION WHERE TIEMPO_TRANSACCION = %s"
+    val = (TimeStamp,)
+    cursora.execute(sql,val)
+    Transaccion = cursora.fetchall()
+    sql = "INSERT INTO USUARIO(RUT_USUARIO, NOMBRE_USUARIO, NOMBRE2_USUARIO, APELLIDO_USUARIO, APELLIDO2_USUARIO, TELEFONO, EMAIL, DIRECCION_USUARIO, FK_TRANSACCION, FK_TIPO_CUENTA, FK_COMUNA, EDAD, CONTRASEÑA) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+    val = (Rut, Nombre, Nombre2, Apellido, Apellido2, Telefono, Email, Direccion, Transaccion, Tipo_cuenta, Comuna, Edad, Contraseña)
+    cursora.execute(sql,val)
+    cursora.connection.commit()
 
-sql = "INSERT INTO TRANSACCION(TIEMPO_TRANSACCION, FK_VALOR_CUENTA) VALUES(%s,%s)"
-val = (TimeStamp,Tipo_cuenta)
-cursora.execute(sql,val)
-cursora.connection.commit()
-sql = "SELECT ID_TRANSACCION FROM TRANSACCION WHERE TIEMPO_TRANSACCION = %s"
-val = (TimeStamp)
-cursora.execute(sql,val)
-Transaccion = cursora.fetchall()
-sql = "INSERT INTO USUARIO(RUT_USUARIO, NOMBRE_USUARIO, NOMBRE2_USUARIO, APELLIDO_USUARIO, APELLIDO2_USUARIO, TELEFONO, EMAIL, DIRECCION_USUARIO, FK_TRANSACCION, FK_TIPO_CUENTA, FK_COMUNA, EDAD, CONTRASEÑA) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-val = (Rut, Nombre, Nombre2, Apellido, Apellido2, Telefono, Email, Direccion, Transaccion, Tipo_cuenta, Comuna, Edad, Contraseña)
-cursora.execute(sql,val)
-cursora.connection.commit()
+    return 'Formulario recibido', 200
 
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
